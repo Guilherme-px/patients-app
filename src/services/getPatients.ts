9fa6951b0553ae556fdd10788d46372c3667800f
Guilherme <guilherme.s.goncalves@outlook.com>
2023-01-01 09:33:52 -0300

    get patient and set in store and loading screen

---
+++ src/services/getPatients.ts
@@ -0,0 +1,20 @@
+import { setLoading, setPatients } from '../store/patient/patientSlice';
+import api from './axios';
+
+const getPatients = async (dispatch: Function) => {
+    dispatch(setLoading(true));
+
+    try {
+        const { data } = await api.get('');
+        console.log(data);
+
+        dispatch(setPatients(data));
+    } catch (err) {
+        if (err instanceof Error) {
+        }
+    }
+
+    dispatch(setLoading(false));
+};
+
+export default getPatients;
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,11 +1,36 @@
+import { useEffect } from 'react';
 import styles from '../assets/sass/button.module.sass';
+import getPatients from '../services/getPatients';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/store';
+import { Box } from '@mui/system';
+import { CircularProgress } from '@mui/material';
 
 const Dashboard = () => {
+    const dispatch = useDispatch();
+
+    const storeState = useSelector((state: RootState) => state.patient);
+
+    useEffect(() => {
+        getPatients(dispatch);
+    }, []);
     return (
         <main className={styles.container}>
-            <div className={styles.button_content}>
-                <button className={styles.button_add}>Novo paciente</button>
-            </div>
+            {storeState.loading ? (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        height: '100vh',
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <div className={styles.button_content}>
+                    <button className={styles.button_add}>Novo paciente</button>
+                </div>
+            )}
         </main>
     );
 };
